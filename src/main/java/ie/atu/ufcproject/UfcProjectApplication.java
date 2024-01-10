e03f09060981823ce5f947c48d8a9c01d609015a
denis <g00403126@atu.ie>
2024-01-10 20:38:00 +0000

    ..

---
+++ src/main/java/ie/atu/ufcproject/UfcProjectApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.ufcproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfcProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UfcProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/ufcproject/UfcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.ufcproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfcProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
